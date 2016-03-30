bd6c878a7a6ce78cf17bb2fca697da90e918712d
wyy <wyy@wyydeimac.local>
2016-03-30 09:51:45 +0800

    Initial Commit

---
+++ XWFunny/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XWFunny
+//
+//  Created by wyy on 16/3/30.
+//  Copyright © 2016年 wyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
